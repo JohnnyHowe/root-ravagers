0b8aef08569079fde6348779479e3eefae6d072f
Johnny Howe <jonathon.o.howe@gmail.com>
2023-02-04 11:15:43 +1300

    Root node rendering

--- Assets/Scripts/RootController.cs
+++ Assets/Scripts/RootController.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class RootController : MonoBehaviour
 {
-    public Vector3 TestNodes;
+    public int MaxSearchRootDepth = 100;
+    public List<RootNode> Leaves;
 
-    public List<RootNode> GetOriginRootNodes() {
-        return new List<RootNode>();
+    void Awake()
+    {
+        // Test data
+        RootNode o1 = new RootNode(Vector3.zero, null);
+        RootNode n1 = new RootNode(Vector3.down, o1);
+        RootNode n2 = new RootNode(new Vector3(1, -2, 0), n1);
+
+        Leaves = new List<RootNode>() { n2 };
+
+        {
+            RootNode origin = new RootNode(new Vector3(-1, 0, 0), null);
+            RootNode node1 = new RootNode(new Vector3(-2, -1, 0), origin);
+            RootNode node2 = new RootNode(new Vector3(-2, -4, 0), node1);
+            RootNode node3 = new RootNode(new Vector3(0, -4, 0), node2);
+            Leaves.Add(node3);
+
+            RootNode b1 = new RootNode(new Vector3(-4, -2, 0), node1);
+            RootNode b2 = new RootNode(new Vector3(-5, -3, 0), b1);
+            Leaves.Add(b2);
+        }
+        // foreach (List<RootNode> nl in GetFullRootPaths()) {
+        //     foreach (RootNode n in nl) {
+        //         Debug.Log(n.Position);
+        //     }
+        //     Debug.Log("");
+        // }
     }
 
     public List<RootNode> GetAllRootNodes()
@@ -15,12 +40,33 @@ public class RootController : MonoBehaviour
         return new List<RootNode>();
     }
 
-    void Update()
+    public List<RootNode> GetLeaves()
     {
-        _DrawRoots();
+        return Leaves;
     }
 
-    private void _DrawRoots() {
+    public List<List<RootNode>> GetFullRootPaths()
+    {
+        List<List<RootNode>> paths = new List<List<RootNode>>();
+        foreach (RootNode leaf in GetLeaves())
+        {
+            List<RootNode> path = new List<RootNode>();
+            RootNode currentNode = leaf;
+            path.Add(currentNode);
 
+            int iteration = 0;
+            while (!currentNode.IsOrigin)
+            {
+                if (iteration > MaxSearchRootDepth)
+                {
+                    Debug.LogError("Max root search depth exceeded, probably a cycle");
+                    break;
+                }
+                currentNode = currentNode.Parent;
+                path.Add(currentNode);
+            }
+            paths.Add(path);
+        }
+        return paths;
     }
 }
