a9f5923f97435b1240b0a9f24d443bc8b11357d9
Johnny Howe <jonathon.o.howe@gmail.com>
2023-02-04 11:56:07 +1300

    Root growth

--- Assets/Scripts/RootController.cs
+++ Assets/Scripts/RootController.cs
@@ -4,36 +4,52 @@ using UnityEngine;
 
 public class RootController : MonoBehaviour
 {
+    public float MasterSpeedMultiplier = 1f;
+    public float GrowSpeedSeconds = 1;
+    private float _timeUntilNextGrowthSeconds = 0;
+
+    public FloatRange NodeDistance = new FloatRange(0.2f, 1);
+
+    public FloatRange NewOriginTimeSeconds = new FloatRange(1, 5);
+    private float _timeUntilNextOriginSeconds = 0;
+
     public int MaxSearchRootDepth = 100;
-    public List<RootNode> Leaves;
+    private List<RootNode> _leaves;
 
     void Awake()
     {
-        // Test data
-        RootNode o1 = new RootNode(Vector3.zero, null);
-        RootNode n1 = new RootNode(Vector3.down, o1);
-        RootNode n2 = new RootNode(new Vector3(1, -2, 0), n1);
-
-        Leaves = new List<RootNode>() { n2 };
-
-        {
-            RootNode origin = new RootNode(new Vector3(-1, 0, 0), null);
-            RootNode node1 = new RootNode(new Vector3(-2, -1, 0), origin);
-            RootNode node2 = new RootNode(new Vector3(-2, -4, 0), node1);
-            RootNode node3 = new RootNode(new Vector3(0, -4, 0), node2);
-            Leaves.Add(node3);
-
-            RootNode b1 = new RootNode(new Vector3(-4, -2, 0), node1);
-            RootNode b2 = new RootNode(new Vector3(-5, -3, 0), b1);
-            Leaves.Add(b2);
-        }
-        // foreach (List<RootNode> nl in GetFullRootPaths()) {
-        //     foreach (RootNode n in nl) {
-        //         Debug.Log(n.Position);
-        //     }
-        //     Debug.Log("");
-        // }
+        _leaves = new List<RootNode>();
     }
+
+    void Update()
+    {
+        if (_leaves.Count > 0)
+        {
+            _timeUntilNextGrowthSeconds -= Time.deltaTime * MasterSpeedMultiplier;
+            if (_timeUntilNextGrowthSeconds <= 0)
+            {
+                int leafIndex = (int) Mathf.Min(_leaves.Count - 1, Random.Range(0f, _leaves.Count));
+                _CreateNewNode(_leaves[leafIndex]);
+                _timeUntilNextGrowthSeconds += GrowSpeedSeconds;
+            }
+
+            // New origins?
+            _timeUntilNextOriginSeconds -= Time.deltaTime * MasterSpeedMultiplier;
+            if (_timeUntilNextOriginSeconds <= 0)
+            {
+                _StartNewOrigin();
+                _timeUntilNextOriginSeconds += NewOriginTimeSeconds.RandomInRange();
+            }
+        }
+        else
+        {
+            _StartNewOrigin();
+        }
+    }
+
+    // ===========================================================================================
+    // Public Methods
+    // ===========================================================================================
 
     public List<RootNode> GetAllRootNodes()
     {
@@ -42,7 +58,7 @@ public class RootController : MonoBehaviour
 
     public List<RootNode> GetLeaves()
     {
-        return Leaves;
+        return _leaves;
     }
 
     public List<List<RootNode>> GetFullRootPaths()
@@ -68,5 +84,39 @@ public class RootController : MonoBehaviour
             paths.Add(path);
         }
         return paths;
+    }
+
+    // ===========================================================================================
+    // Root generation/growth
+    // ===========================================================================================
+
+    /// <summary>
+    /// Create new root node, add it to leaves, return reference
+    /// </summary>
+    private RootNode _StartNewOrigin()
+    {
+        RootNode node = new RootNode(_GetValidOriginPosition(), null);
+        _leaves.Add(node);
+        return node;
+    }
+
+    private Vector3 _GetValidOriginPosition()
+    {
+        return new Vector3(Random.Range(-5f, 5f), 0, 0);
+    }
+
+    private RootNode _CreateNewNode(RootNode parent)
+    {
+        RootNode node = new RootNode(_GetNextNodePosition(parent), parent);
+        _leaves.Remove(parent);
+        _leaves.Add(node);
+        return node;
+    }
+
+    private Vector3 _GetNextNodePosition(RootNode parent)
+    {
+        // TODO smarter pos choice - angles maybe?
+        Vector3 dir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 0), 0).normalized;
+        return parent.Position + dir * NodeDistance.RandomInRange();
     }
 }
