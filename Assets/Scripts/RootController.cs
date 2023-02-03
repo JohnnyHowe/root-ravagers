e9ad7d1abd81f08ba9d3618a4464bde410e070f9
Johnny Howe <jonathon.o.howe@gmail.com>
2023-02-04 10:25:25 +1300

    RootController empty public methods

---
+++ Assets/Scripts/RootController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootController : MonoBehaviour
+{
+    public List<RootNode> GetRootNodes() {
+        return new List<RootNode>();
+    }
+}
---
+++ Assets/Scripts/RootNode.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class RootNode
+{
+    public RootNode parent;
+    public Vector3 Position;
+}
