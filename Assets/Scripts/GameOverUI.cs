a8f2431c2543614c7471db7f0b0b58bc80ef0995
Johnny Howe <jonathon.o.howe@gmail.com>
2023-02-04 21:17:15 +1300

    can set usetname

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -10,19 +10,35 @@ public class GameOverUI : MonoBehaviour
     public Transform UIContainer;
     private GameController _gameController;
     public Transform HighScoreEntryPrototype;
+    private bool _scoresPopulated = false;
+    private Persistence _persistence;
+    Transform currentUserHighScoreEntry;
 
     void Start()
     {
-        UIContainer.gameObject.SetActive(false);
+        // UIContainer.gameObject.SetActive(false);
         _gameController = GameObject.FindObjectOfType<GameController>();
+        _persistence = GameObject.FindObjectOfType<Persistence>();
         HighScoreEntryPrototype.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if (_gameController.IsGameOver())
+        // if (_gameController.IsGameOver() && !_scoresPopulated)
+        if (!_scoresPopulated)
         {
+            _scoresPopulated = true;
             UIContainer.gameObject.SetActive(true);
+            PopulateLeaderboard(_persistence.Data.Scores);
+            _persistence.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TMP_InputField inputField = currentUserHighScoreEntry.GetChild(3).GetComponent<TMP_InputField>();
+            _persistence.Data.Scores[inputField.text] = 10;
+            inputField.gameObject.SetActive(false);
+            currentUserHighScoreEntry.GetChild(1).gameObject.SetActive(true);
+            currentUserHighScoreEntry.GetChild(1).GetComponent<TextMeshProUGUI>().text = inputField.text;
         }
     }
 
@@ -33,14 +49,34 @@ public class GameOverUI : MonoBehaviour
 
     public void PopulateLeaderboard(Dictionary<string, int> highScores)
     {
-        foreach (var (name, score) in highScores.OrderBy(a => a.Value)) {
+        // Inject current users'
+        string currentUserName = "CURRENTUSER1234567890";
+        int currentScore = 10;
+        highScores[currentUserName] = currentScore;
+        highScores["aa"] = currentScore;
+        highScores["ab"] = currentScore;
+        highScores["ac"] = currentScore;
+
+        foreach (var (name, score) in highScores.OrderBy(a => a.Value))
+        {
             Transform highScoreEntryUI = _GetNewHighScoreEntry();
             highScoreEntryUI.GetChild(1).GetComponent<TextMeshProUGUI>().text = name;
             highScoreEntryUI.GetChild(2).GetComponent<TextMeshProUGUI>().text = score.ToString();
+            if (name == currentUserName && score == currentScore)
+            {
+                highScoreEntryUI.GetChild(3).gameObject.SetActive(true);
+                highScoreEntryUI.GetChild(1).gameObject.SetActive(false);
+                currentUserHighScoreEntry = highScoreEntryUI;
+            }
+            else
+            {
+                highScoreEntryUI.GetChild(3).gameObject.SetActive(false);
+            }
         }
     }
 
-    private Transform _GetNewHighScoreEntry() {
+    private Transform _GetNewHighScoreEntry()
+    {
         Transform h = Instantiate(HighScoreEntryPrototype);
         h.parent = HighScoreEntryPrototype.parent;
         h.gameObject.SetActive(true);
