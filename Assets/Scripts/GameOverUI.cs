ac82e6ebba89e295103932f62e4ae1d5d6df2467
Johnny Howe <jonathon.o.howe@gmail.com>
2023-02-04 16:19:29 +1300

    restart butt work

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
@@ -23,5 +24,6 @@ public class GameOverUI : MonoBehaviour
 
     public void OnReplayPress()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
