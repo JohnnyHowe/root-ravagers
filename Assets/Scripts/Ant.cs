248070bbed6cdd8e3014bb1f24254decb780d7dd
Robert Oram <roboenator.corp@gmail.com>
2023-02-04 20:09:51 +1300

    This is not better

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -8,6 +8,11 @@ public class Ant : MonoBehaviour
     public float MinSpeed = 1.0f;
     public float Speed = 5;
     private AntGroup _antGroup;
+
+    private float _currentSpeed = 0;
+    private float _currentRotation = 0;
+    private float _timeToNextCheck = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,28 +22,40 @@ public class Ant : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var delta = _antGroup.transform.position - transform.position;
-        //var asdf = transform.rotation * Vector3.up;
-        //Vector3.Dot(delta.normalized, transform.up);
-        var fff = Vector3.SignedAngle(delta.normalized, transform.up, Vector3.forward);
 
-        //var a = transform.rotation.eulerAngles;
-        //a.x += fff > 0 ? -1f : 1f;
-        //transform.rotation = Quaternion.Euler(a);
-        if (fff > Random.Range(20, 120))
+        transform.Rotate(0, 0, _currentRotation * Time.deltaTime);
+        transform.position += transform.up * _currentSpeed * Time.deltaTime;
+
+        _timeToNextCheck -= Time.deltaTime;
+
+        var delta = _antGroup.transform.position - transform.position;
+        var signedAngle = Vector3.SignedAngle(delta.normalized, transform.up, Vector3.forward);
+
+        if (Mathf.Abs(signedAngle) > 90)
         {
-            transform.Rotate(0, 0, -Random.Range(100, 10000) * Time.deltaTime);
-        } else if (fff < -Random.Range(20, 120))
-        {
-            transform.Rotate(0, 0, Random.Range(100, 10000) * Time.deltaTime);
+            _timeToNextCheck = 0;
         }
 
-        //transform.LookAt(_antGroup.transform.position, Vector3.up);
+
+        if (_timeToNextCheck > 0)
+        {
+            return;
+        }
+
+
+        _timeToNextCheck = Random.Range(0.01f, 0.5f);
+
+
+        if (signedAngle > Random.Range(1, 30))
+        {
+            _currentRotation = -Random.Range(1, 2000);
+        } else if (signedAngle < -Random.Range(1, 120))
+        {
+            _currentRotation = Random.Range(1, 2000);
+        }
+
 
         var distance = delta.magnitude;
-
-        float movement = System.Math.Min(Speed, distance/DistanceMultiplyer + MinSpeed);
-
-        transform.position += transform.up * movement * Time.deltaTime;
+        _currentSpeed = System.Math.Min(Speed * Random.Range(0.5f, 2f), distance * Random.Range(0.5f, 2f) + MinSpeed);
     }
 }
